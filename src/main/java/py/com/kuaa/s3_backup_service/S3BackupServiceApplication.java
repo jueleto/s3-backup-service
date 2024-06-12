b68db5a32f1cfe80927dfaa7155e3bd494c0fe05
Juan E <jueleto@gmail.com>
2024-06-12 19:25:52 -0400

    first commit

---
+++ src/main/java/py/com/kuaa/s3_backup_service/S3BackupServiceApplication.java
@@ -0,0 +1,13 @@
+package py.com.kuaa.s3_backup_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3BackupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3BackupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/com/kuaa/s3_backup_service/S3BackupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.kuaa.s3_backup_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3BackupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
