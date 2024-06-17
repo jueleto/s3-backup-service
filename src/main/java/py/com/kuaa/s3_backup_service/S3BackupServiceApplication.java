0429ca62971f6a7da2e9a5f9e408360c9aa10ad5
Juan E <jueleto@gmail.com>
2024-06-17 09:32:09 -0400

    ajustando detalles de presentacion

--- src/main/java/py/com/kuaa/s3_backup_service/S3BackupServiceApplication.java
+++ src/main/java/py/com/kuaa/s3_backup_service/S3BackupServiceApplication.java
@@ -3,11 +3,22 @@ package py.com.kuaa.s3_backup_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
 @SpringBootApplication
 public class S3BackupServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(S3BackupServiceApplication.class, args);
 	}
+
+
+
+
+	
+
+
 
 }
--- src/main/java/py/com/kuaa/s3_backup_service/service/RunnerInicial.java
+++ src/main/java/py/com/kuaa/s3_backup_service/service/RunnerInicial.java
@@ -1,10 +1,18 @@
 package py.com.kuaa.s3_backup_service.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.jar.Manifest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
+import py.com.kuaa.s3_backup_service.S3BackupServiceApplication;
 import py.com.kuaa.s3_backup_service.s3.S3OperationInterface;
 
 @Slf4j
@@ -16,6 +24,9 @@ public class RunnerInicial implements CommandLineRunner {
 
     @Autowired
     S3OperationInterface bucketOperation;
+
+    @Value("${spring.application.name}")
+	private String springApplicationName;
 
     // @Autowired
     public RunnerInicial() {
@@ -45,7 +56,7 @@ public class RunnerInicial implements CommandLineRunner {
         String filePath = args[0];
         //String directorioDestino = args[1];
 
-
+        mensajeInicio();
         procesadorArchivo.leer(filePath);
         procesadorArchivo.procesar();
 
@@ -53,4 +64,36 @@ public class RunnerInicial implements CommandLineRunner {
         //servicioInicial.miMetodo(argumento1, argumento2);
         
     }
+
+    public void mensajeInicio() {
+		Date date = new Date();
+		String DD_MM_AAAA=new SimpleDateFormat("dd/MM/yyyy").format(date);
+		String HH_MM_SS=new SimpleDateFormat("HH:mm:ss").format(date);
+
+
+		String implementationVersion = null;
+        try (InputStream is = S3BackupServiceApplication.class.getResourceAsStream("/META-INF/MANIFEST.MF")) {
+			implementationVersion = new Manifest(is).getMainAttributes().getValue("Implementation-Version");
+		} catch (IOException e) {
+			log.warn("No se pudo obtener información del archivo MANIFEST.MF", e);
+		}
+
+		System.out.println("\n           ▄█████▄");
+		System.out.println("        ▄███████████▄");
+		System.out.println("     ▄██████       █████▄");
+        System.out.println("    ██████   LISTO   █████");
+        System.out.println("   █████               ████");
+        System.out.println("  ████   "+"EJECUTANDOSE"+"   ████");
+        System.out.println("   ████   "+DD_MM_AAAA+"   ████");
+        System.out.println("    ████▄  "+HH_MM_SS+"  ▄████");
+        System.out.println("       ███         ▄███");
+        System.out.println("         ███████████"); 
+		System.out.println("            █████ \n"); 
+
+		System.out.println("\n" + springApplicationName);
+		System.out.println("Versión: "+implementationVersion);
+	
+		System.out.println("\n\n");
+
+	}
 }
